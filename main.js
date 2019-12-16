1d13fab2d5e39a5a968707300e8984b5264413ec
athagi <at.hgt.dev@gmail.com>
2019-12-17 08:30:51 +0900

    1st commit

---
+++ main.js
@@ -0,0 +1,58 @@
+var filledColor = "#ff00ff"
+
+var input = [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+function display(canvas) {
+    //描画コンテキストの取得
+    // var canvas = document.getElementById('base');
+    if (canvas.getContext) {
+        var context = canvas.getContext('2d');
+        //ここに具体的な描画内容を指定する
+        //線スタイルに青色を指定する
+        context.strokeStyle = "rgb(0, 0, 255)";
+        //左から20上から20の位置に幅50高さ50の輪郭の四角形を描く
+        var x = 0,
+            y = 0,
+            z = 30;
+        for (var i = 0; i < 30; i++) {
+            for (var j = 0; j < 30; j++) {
+                // context.strokeRect(x, y, z, z);
+                drawBox(context, x, y, z, input[j] == 1 ? true : false);
+                x += z;
+            }
+            y += z;
+            x = 0;
+
+        }
+    }
+}
+
+function drawBox(context, x, y, z, filled) {
+    console.log(filled)
+    if (filled) {
+        context.fillStyle = filledColor
+        context.fillRect(x, y, z, z)
+    } else {
+        context.strokeRect(x, y, z, z);
+    }
+}
+
+function getDisplay() {
+    width = window.screen.availWidth;
+    height = window.screen.availHeight;
+
+}
+
+function drawBase(canvas) {
+
+    const ctx = canvas.getContext('2d');
+
+    ctx.fillStyle = 'floralwhite';
+    ctx.fillRect(0, 0, 900, 900);
+}
+
+function main() {
+    const canvas = document.getElementById('base');
+    drawBase(canvas);
+    display(canvas);
+}
